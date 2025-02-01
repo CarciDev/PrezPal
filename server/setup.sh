3dbeeb13f3f0de0005239fb0baee1a54cb2e7b7f
mahutt <tommy.mahut@gmail.com>
2025-02-01 18:13:19 -0500

    Delete node_modules lol

---
+++ server/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Preconditions
+# 1. pyenv is installed
+# 2. `pyenv install 3.11` was run
+# 3. git creds are setup on command line (ssh setup)
+
+# JUST THE COMMANDS FOR REFERENCE
+uv venv env --python=python3.11
+source env/bin/activate
+export KERAS_BACKEND=torch
+uv pip install https://github.com/usefulsensors/moonshine.git
+
+git clone git@github.com:usefulsensors/moonshine.git
+uv pip install numba
+uv pip install -r moonshine/demo/moonshine-onnx/requirements.txt
+
+uv pip install Flask flask_socketio eventlet flask_cors
+
+# demo
+# python3 moonshine/demo/moonshine-onnx/live_captions.py
+ 
