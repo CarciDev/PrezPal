135ef0ccaca464c22dcb0509937ba8e7dcadadad
mahutt <tommy.mahut@gmail.com>
2025-02-01 21:56:09 -0500

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -5,7 +5,7 @@
 # 2. `pyenv install 3.11` was run
 # 3. git creds are setup on command line (ssh setup)
 
-# JUST THE COMMANDS FOR REFERENCE
+# JUST THE COMMANDS FOR REFERENCE - (RUN IN SERVER FOLDER)
 uv venv env --python=python3.11
 source env/bin/activate
 export KERAS_BACKEND=torch
@@ -15,7 +15,7 @@ git clone git@github.com:usefulsensors/moonshine.git
 uv pip install numba
 uv pip install -r moonshine/demo/moonshine-onnx/requirements.txt
 
-uv pip install Flask flask_socketio eventlet flask_cors
+uv pip install Flask flask_cors
 
 # demo
 # python3 moonshine/demo/moonshine-onnx/live_captions.py
